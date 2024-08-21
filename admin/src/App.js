f40fe12f569fb5e7a4dff6bdc5a4e4cb9475012a
SAI KUMAR@LAPTOP-52G4EK29 <saikumarkaicherla2004@gmail.com>
2024-08-21 21:22:54 +0530

    update

--- admin/src/App.js
+++ admin/src/App.js
@@ -8,7 +8,7 @@ import ListSong from './pages/ListSong';
 import ListAlbum from './pages/ListAlbum';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
-export const url='http://localhost:4002'
+export const url='https://spotify-clone-mernstack-1.onrender.com'
 function App() {
   return (
     <div className="flex items-start min-h-screen">
