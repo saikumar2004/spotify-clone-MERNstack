d00c4d8f5ac3ce40b60b62102eaaa17d30475708
SAI KUMAR@LAPTOP-52G4EK29 <saikumarkaicherla2004@gmail.com>
2024-08-27 10:45:16 +0530

    route protection update

--- client/src/components/display.js
+++ client/src/components/display.js
@@ -5,13 +5,14 @@ import DisplayAlbum from './DisplayAlbum';
 import { PlayerContext } from '../context/PlayerContext';
 import Songslist from './songlist';
 import Private from './Private'
-import NotFound from './NotFound';
+// import NotFound from './NotFound';
 function Display() {
   const { albumsData } = useContext(PlayerContext);
   const displayRef = useRef();
   const location = useLocation();
   const isAlbum = location.pathname.includes("album");
   const albumId = isAlbum ? location.pathname.split('/').pop() : "";
+  const album = isAlbum && albumsData.length > 0 ? albumsData.find((x) => x._id === albumId) : null;
   const bgColour = isAlbum && albumsData.length > 0 ? albumsData.find((x) => (x._id === albumId)).bgColor : "#121212"
   useEffect(() => {
     if (isAlbum) {
@@ -20,7 +21,7 @@ function Display() {
     else {
       displayRef.current.style.background = `#121212`;
     }
-  })
+  }, [isAlbum, album, bgColour])
   return (
     <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       {albumsData.length > 0
@@ -29,8 +30,15 @@ function Display() {
           {/* <Route path='/home' element={<DisplayHome />} /> */}
           <Route path='/home' element={<Private  Component={DisplayHome} />} />
     <Route path='/songslist' element={<Private Component={Songslist}/>} />
-    <Route path='/album/:id' element={<Private Component={<DisplayAlbum album={albumsData.find((x) => (x._id === albumId))}/>} />}  />
-<Route path='*' element={<NotFound/>}/>
+    <Route 
+    path='/album/:id' 
+    element={
+        <Private 
+            Component={DisplayAlbum} 
+            album={album}
+        />    } 
+/>
+{/* <Route path='*' element={<NotFound/>}/> */}
           {/* <Route path='/songslist' element={<Songslist/>} /> */}
           {/* <Route path='/album/:id' element={<DisplayAlbum album={albumsData.find((x) => (x._id === albumId))} />} /> */}
         </Routes>
